64a41e41ef93bf06b2ec364aa5657a0ee06aaab5
wangshilian@gmail.com <wangshilian@gmail.com>
2014-10-07 16:56:35 +0800

    server

--- startup.sh
+++ startup.sh
@@ -1,1 +1,15 @@
-sudo ../ejre1.7.0_40/bin/java -cp .:lib/*:conf http.startup.JettyServer
+#! /bin/bash 
+[ -z $BAG_HOME ] && BAG_HOME=`cd ..;pwd`
+echo "BAG_HOME : "$BAG_HOME
+
+BAG_LOG=$BAG_HOME/logs
+BAG_OUT=$BAG_LOG/bag.out
+
+if [ ! -d "$BAG_LOG" ]; then
+  mkdir $BAG_LOG
+fi
+
+touch "$BAG_OUT"
+
+cd ..
+java -cp .:lib/*:conf http.startup.JettyServer >> "$BAG_OUT" 2>&1 &
